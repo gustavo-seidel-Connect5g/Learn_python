20570c5418123c618dea4a6cca08b0e424ac7556
gustavo-seidel <tecnico.eletrnico@yahoo.com.br>
2022-11-16 21:49:48 -0300

    Creat boot version offile

---
+++ Boot_Off/boot.py
@@ -0,0 +1,23 @@
+import pyautogui
+
+# This is progam will autofill a sheet for make a LTE-NR relation.
+pyautogui.PAUSE = 2
+
+# Open the tool
+pyautogui.click(x=637, y=747)
+pyautogui.click(x=64, y=583)
+pyautogui.doubleClick(x=217, y=176)
+pyautogui.doubleClick(x=203, y=233)
+pyautogui.doubleClick(x=229, y=130)
+pyautogui.doubleClick(x=212, y=130)
+pyautogui.doubleClick(x=221, y=125)
+
+pyautogui.
+
+#Point(x=637, y=747) - explorador de arquivos
+#Point(x=1482, y=575) - Area de trabalho
+#Point(x=1603, y=174) - Connect
+#Point(x=1609, y=230)) - trabalho
+#Point(x=1643, y=132) - alt-tool
+#Point(x=1634, y=130) - FCI
+#Point(x=1610, y=135) - tool
